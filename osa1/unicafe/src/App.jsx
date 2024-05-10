9e8b62451ca3198ba5797ad48830b1b99a718b06
AnttiVainikka <antti.vainikka36@gmail.com>
2024-05-10 12:50:33 +0300

    Finished 1.7

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  const all = good+neutral+bad
   return (
     <div>
       <h1>give feedback</h1>
@@ -21,7 +21,10 @@ const App = () => {
       <p>
         good {good}<br></br>
         neutral {neutral}<br></br>
-        bad {bad}
+        bad {bad}<br></br>
+        all {all}<br></br>
+        average {(good-bad)/all}<br></br>
+        positive {good/all*100} %
       </p>
     </div>
   )
