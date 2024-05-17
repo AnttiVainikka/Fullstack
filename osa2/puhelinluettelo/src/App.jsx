15cee195ef4a538320db019fddd5cd528b439f55
AnttiVainikka <antti.vainikka36@gmail.com>
2024-05-17 15:04:11 +0300

    Finished 2.11

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const RenderPersons = (props) => {
   let persons = props.persons
@@ -51,16 +52,19 @@ const AddPersons = ({addPerson,newName,handleNameChange,newNumber,handleNumberCh
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
   const [activeFilter, setActiveFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -97,7 +101,7 @@ const App = () => {
       {AddPersons({addPerson,newName,handleNameChange,newNumber,handleNumberChange})}
       
       <h3>Numbers</h3>
-      
+
       <RenderPersons persons={persons} filter={activeFilter}/>
     </div>
   )
