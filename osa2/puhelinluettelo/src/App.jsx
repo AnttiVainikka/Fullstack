758828434d3964292e2b47c37454073a16e4f230
AnttiVainikka <antti.vainikka36@gmail.com>
2024-05-17 13:34:53 +0300

    2.9 finished

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 
+const RenderPersons = (props) => {
+  let persons = props.persons
+  if (props.filter) {
+    persons = persons.filter((person) => person.name.toLowerCase().includes(props.filter))
+  }
+  return persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newFilter, setNewFilter] = useState('')
+  const [activeFilter, setActiveFilter] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -18,6 +31,11 @@ const App = () => {
     setNewNumber('')
   }
 
+  const filterPerson = (event) => {
+    event.preventDefault()
+    setActiveFilter(newFilter.toLowerCase())
+  }
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
@@ -26,9 +44,21 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={filterPerson}>
+        filter shown with
+        <input
+        value={newFilter}
+        onChange={handleFilterChange}
+        />
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: 
@@ -49,7 +79,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      <RenderPersons persons={persons} filter={activeFilter}/>
     </div>
   )
 
